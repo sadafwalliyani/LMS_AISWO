e9ad14897a82cc35fe5d0a7c2bfc5ccdc0f2ce89
sadafwalliyani <sadafwalliyani@gmail.com>
2025-02-15 20:00:34 +0500

    done

--- myenv/Scripts/activate.bat
+++ myenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\Library_managemrnta-system\myenv
+set VIRTUAL_ENV=D:\LMS_AISWO\myenv
 
 if not defined PROMPT set PROMPT=$P$G
 
